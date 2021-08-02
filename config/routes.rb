662ff37dd675a06b8faf8060a47880d043a866c1
daniels <daniels.kor@gmail.com>
2021-08-03 00:20:09 +0300

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :questions, only: %i[index new edit create update destroy show]
+
   root 'pages#index'
 
-  get '/questions', to: 'questions#index'
 end
